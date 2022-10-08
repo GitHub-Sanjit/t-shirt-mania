a3c77beaa9a444ec526ac97469482023fc3d12d5
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-08 23:01:16 +0600

    Context API practice

--- t-shirt-mania/src/components/TShirt/TShirt.js
+++ t-shirt-mania/src/components/TShirt/TShirt.js
@@ -1,16 +1,16 @@
-import React from "react";
-import "./TShirt.css";
+import React from 'react';
+import './TShirt.css';
 
-const TShirt = ({ tshirt }) => {
-  const { picture, name, price } = tshirt;
-  return (
-    <div className="t-shirt">
-      <img src={picture} alt="alt Img" />
-      <h2>{name} </h2>
-      <h4>Price: {price} </h4>
-      <button>Buy This</button>
-    </div>
-  );
+const TShirt = ({tshirt, handleAddToCart}) => {
+    const {picture, name, price} = tshirt;
+    return (
+        <div className='t-shirt'>
+            <img src={picture} alt=""></img>
+            <h2>{name}</h2>
+            <h4>Price: {price}</h4>
+            <button onClick={() => handleAddToCart(tshirt)}>Buy this</button>
+        </div>
+    );
 };
 
 export default TShirt;
